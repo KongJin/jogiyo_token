95b2229c9a35709997c31cbb6d84c4b00f6dc066
codingbe <descuento@naver.com>
2022-01-08 18:38:13 +0900

    Fixed: json 변경

--- src/controllers/kakaoController.js
+++ src/controllers/kakaoController.js
@@ -2,26 +2,31 @@ import axios from "axios";
 
 export const postKakaoToken = async (req, res) => {
   const { code } = req.body;
-  if (!code) {
-    return res.json({ message: "not have code" });
-  } else {
-    const {
-      data: { access_token },
-    } = await axios.post(
-      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=http://localhost:3000&code=${code}`
-    );
-    const {
-      data: {
-        id,
-        properties: { nickname },
-      },
-    } = await axios.get("https://kapi.kakao.com/v2/user/me", {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-      },
-    });
 
-    return res.json({ id, nickname });
+  if (!code) {
+    return res.json({ message: "Not Authorized" });
+  } else {
+    try {
+      const {
+        data: { access_token },
+      } = await axios.post(
+        `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=http://localhost:3000&code=${code}`
+      );
+      // const {
+      //   data: {
+      //     id,
+      //     properties: { nickname },
+      //   },
+      // } = await axios.get("https://kapi.kakao.com/v2/user/me", {
+      //   headers: {
+      //     Authorization: `Bearer ${access_token}`,
+      //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+      //   },
+      // });
+
+      return res.json({ accessToken: access_token });
+    } catch {
+      return res.json({ message: "Not Authorized" });
+    }
   }
 };
